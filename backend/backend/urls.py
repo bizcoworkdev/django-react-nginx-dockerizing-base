9217af6f272476f34c69d4f8fd63cbd7dbc5fe09
unknown <dragonsuperf@gmail.com>
2020-12-08 11:47:56 +0900

    first commit

---
+++ backend/backend/urls.py
@@ -0,0 +1,34 @@
+"""backend URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework.urlpatterns import format_suffix_patterns
+from api import views
+from rest_framework.documentation import include_docs_urls
+
+docs_view = include_docs_urls( title='drf API', description='API document' )
+
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('api/admin', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('api/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/docs', docs_view),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
