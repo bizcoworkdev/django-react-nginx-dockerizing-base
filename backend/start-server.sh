05c6006c55f9b06575c5187ea28a88926586af81
unknown <dragonsuperf@gmail.com>
2020-12-11 15:15:46 +0900

    django test 환경으로 설정 변경

--- backend/start-server.sh
+++ backend/start-server.sh
@@ -2,4 +2,5 @@
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-gunicorn backend.wsgi:application --bind 0.0.0.0:8000 --workers 3
+python manage.py runserver 0.0.0.0:8000
+#gunicorn backend.wsgi:application --bind 0.0.0.0:8000 --workers 3
